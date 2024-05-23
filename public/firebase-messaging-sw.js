3422713a7048773a054defbca2ccaf6673f1ee67
kimbamtol0304 <kimbamtol0304@gmail.com>
2024-05-23 23:37:57 +0900

    firebase messaging 수정

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBe6FveuA-2cUArXLNS6jVxDQjeraHPAAQ",
+    authDomain: "drug-a3da8.firebaseapp.com",
+    projectId: "drug-a3da8",
+    storageBucket: "drug-a3da8.appspot.com",
+    messagingSenderId: "1024950698087",
+    appId: "1:1024950698087:web:aab9f0e4e2cd9b381e14c2",
+    measurementId: "G-J5PS5H6L9R"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
